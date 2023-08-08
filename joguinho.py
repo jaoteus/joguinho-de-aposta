32800713e35322564848b5744bbf462c44c9b7a1
Mateus <mateusff2323@gmail.com>
2023-08-08 20:44:06 -0300

    atualização

--- joguinho.py
+++ joguinho.py
@@ -1,6 +1,6 @@
 import random
 # Variáveis
-saldo = 0.0; valor = 0.0; nome = ''; idade = ''; numAleatorio = 0; VALORPERCA = 1.0; VALORGANHO = 2.0; numEscolhido = ''
+saldo = 0.0; valor = 0.0; nome = ''; idade = ''; numAleatorio = 0; VALORPERCA = 0.50; VALORGANHO = 2.0; numEscolhido = ''
 # loops
 loopCriarConta = loopNome = lacoIdade = loopDeposito = loopAposta = True
 loopPrincipal = True
@@ -18,7 +18,6 @@ try:
                 nome = '{} {}'
                 nome = nome.format(primeiroNome, sobreNome)
                 break
-#------------------------------------------
         while lacoIdade:
             idade = input('Digite sua idade: ')
             if idade.isnumeric() == False:
@@ -32,50 +31,54 @@ try:
                 loopCriarConta = False
                 loopPrincipal = False
                 lacoIdade = False
-            elif idade > 18:
-                print('Conta criada com sucesso!')
+            elif idade >= 18:
+                print('\nConta criada com sucesso!\n')
                 break
-            #-----------------------------------------------
         break
     # Funções
      
     def main():
-        print('1 - Ver seu dados\n2 - Iniciar aposta\n3 - Depositar\nS / s - Sair\n')
+        print('1 - Ver seu dados\n2 - Iniciar aposta\n3 - Depositar\n4 - Sacar\nS / s - Sair\n')
 
     def mostrarDados():
-            mostrarNome = 'Seu nome é {}'
+            mostrarNome = '\nSeu nome é {}'
             mostrarNome = mostrarNome.format(nome)
             print(mostrarNome)
             mostrarIdade = 'Sua idade é {}'
             mostrarIdade = mostrarIdade.format(idade)
             print(mostrarIdade)
-            mostrarSaldo = 'Seu saldo é de R${}'
+            mostrarSaldo = 'Seu saldo é de R${}\n'
             mostrarSaldo = mostrarSaldo.format(saldo)
             print(mostrarSaldo)
 
+#------------------------------------------------------
     def aposta():
         global saldo, VALORGANHO, VALORPERCA
-        print('Ganho: R$2.00')
+        print('\n!!! Se você acertar o número aleatório, ganhará R$2.00 !!!\n!!! Se você não acertar o número aleatório, perderá R$0.50 !!!')
         while loopAposta:
-            numAleatorio = random.randrange(0, 3)
+            numAleatorio = random.randrange(0, 2)
             numAleatorio = str(numAleatorio)
-            numEscolhido = input('Digite um número entre 0 e 3: ')
+            numEscolhido = input('\nDigite um número de 0 a 2: ')
             if numAleatorio == numEscolhido:
                 saldo += VALORGANHO
-                print('Você ganhou :)')
+                print('\nVocê ganhou :)')
                 mensagemGanho = 'Depositamos um valor de R${} na sua conta, seu saldo atual é de R${}\n'
                 mensagemGanho = mensagemGanho.format(VALORGANHO, saldo)
                 print(mensagemGanho)
                 break
             elif numAleatorio != numEscolhido:
-                print('Você perdeu :(')
+                saldo -= VALORPERCA
+                print('\nVocê perdeu :(')
+                mensagemPerda = 'Retiramos R${} da sua conta, seu saldo atual é R${}\n'
+                mensagemPerda = mensagemPerda.format(VALORPERCA, saldo)
+                print(mensagemPerda)
                 break
-
+#-----------------------------------------------------------------
     def depositar():
         global valor, saldo
-        valor = float(input('Digite o valor que deseja depositar: R$'))
+        valor = float(input('\nDigite o valor que deseja depositar: R$'))
         saldo += valor
-        mostrarValorDepositado = 'Valor de R${} depositado com sucesso!\nSeu saldo agora é de R${}'
+        mostrarValorDepositado = 'Valor de R${} depositado com sucesso!\nSeu saldo agora é de R${}\n'
         mostrarValorDepositado = mostrarValorDepositado.format(valor, saldo)
         print(mostrarValorDepositado)
 
@@ -84,8 +87,16 @@ try:
         opcaoMain = input('Digite uma opção: ')
         if opcaoMain == '1':
             mostrarDados()
+        #---------------------------
         elif opcaoMain =='2':
-            aposta()
+            if saldo <= 0: # --> passou
+                print('\nVocê não tem dinheiro suficiente para apostar, deposite uma quantia em R$.')
+                mostrarSaldo = 'Seu saldo atual é de R${}\n'
+                mostrarSaldo = mostrarSaldo.format(saldo)
+                print(mostrarSaldo)
+            else:
+                aposta()
+        #--------------------------
         elif opcaoMain == '3':
             depositar()
         elif opcaoMain == 'S' or opcaoMain == 's':
@@ -93,4 +104,4 @@ try:
             loopPrincipal = False
 
 except:
-    print('Algo deu errado :( ')
+    print('Algo deu errado :(')
