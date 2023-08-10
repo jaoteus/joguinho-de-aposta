a43008059173bbe5619a2d41b56e722a426bc562
Mateus <mateusff2323@gmail.com>
2023-08-10 08:54:49 -0300

    atualização

--- joguinho.py
+++ joguinho.py
@@ -1,8 +1,9 @@
+# módulos
 import random
 # Variáveis
 saldo = 0.0; valor = 0.0; nome = ''; idade = ''; numAleatorio = 0; VALORPERCA = 0.50; VALORGANHO = 2.0; numEscolhido = ''
 # loops
-loopCriarConta = loopNome = lacoIdade = loopDeposito = loopAposta = True
+loopCriarConta = loopNome = lacoIdade = loopDeposito = loopAposta = loopSacar = True
 loopPrincipal = True
 try:
     while loopCriarConta:
@@ -41,24 +42,24 @@ try:
         print('1 - Ver seu dados\n2 - Iniciar aposta\n3 - Depositar\n4 - Sacar\nS / s - Sair\n')
 
     def mostrarDados():
-            mostrarNome = '\nSeu nome é {}'
-            mostrarNome = mostrarNome.format(nome)
-            print(mostrarNome)
-            mostrarIdade = 'Sua idade é {}'
-            mostrarIdade = mostrarIdade.format(idade)
-            print(mostrarIdade)
-            mostrarSaldo = 'Seu saldo é de R${}\n'
-            mostrarSaldo = mostrarSaldo.format(saldo)
-            print(mostrarSaldo)
+        global mostrarNome,  mostrarIdade, mostrarSaldo
+        mostrarNome = '\nSeu nome é {}'
+        mostrarNome = mostrarNome.format(nome)
+        print(mostrarNome)
+        mostrarIdade = 'Sua idade é {}'
+        mostrarIdade = mostrarIdade.format(idade)
+        print(mostrarIdade)
+        mostrarSaldo = 'Seu saldo é de R${}\n'
+        mostrarSaldo = mostrarSaldo.format(saldo)
+        print(mostrarSaldo)
 
-#------------------------------------------------------
     def aposta():
-        global saldo, VALORGANHO, VALORPERCA
+        global saldo, VALORGANHO, VALORPERCA, numAleatorio, numEscolhido, mensagemGanho, mensagemPerda
         print('\n!!! Se você acertar o número aleatório, ganhará R$2.00 !!!\n!!! Se você não acertar o número aleatório, perderá R$0.50 !!!')
         while loopAposta:
-            numAleatorio = random.randrange(0, 2)
+            numAleatorio = random.randrange(0, 3)
             numAleatorio = str(numAleatorio)
-            numEscolhido = input('\nDigite um número de 0 a 2: ')
+            numEscolhido = input('\nDigite um número de 0 a 3: ')
             if numAleatorio == numEscolhido:
                 saldo += VALORGANHO
                 print('\nVocê ganhou :)')
@@ -73,35 +74,63 @@ try:
                 mensagemPerda = mensagemPerda.format(VALORPERCA, saldo)
                 print(mensagemPerda)
                 break
-#-----------------------------------------------------------------
     def depositar():
-        global valor, saldo
+        global valor, saldo, mostrarValorDepositado
         valor = float(input('\nDigite o valor que deseja depositar: R$'))
         saldo += valor
         mostrarValorDepositado = 'Valor de R${} depositado com sucesso!\nSeu saldo agora é de R${}\n'
         mostrarValorDepositado = mostrarValorDepositado.format(valor, saldo)
         print(mostrarValorDepositado)
 
+    def sacar():
+        global loopSacar, valor, saldo, mostrarSaldo, mostrarValorSacado
+        if saldo == 0.0:
+            print('Você não tem dinheiro suficiente para sacar :(')
+            mostrarSaldo = 'Seu saldo atual é de R${}'
+            mostrarSaldo = mostrarSaldo.format(saldo)
+            print(mostrarSaldo)
+        else:
+            pass
+        while loopSacar:
+            valor = float(input('Digite o valor que deseja sacar: R$'))
+            if valor > saldo:
+                print('Saldo insuficiente :(')
+                mostrarSaldo = 'Seu saldo atual é {}'
+                mostrarSaldo = mostrarSaldo.format(saldo)
+                print(mostrarSaldo)
+                break
+            else:
+                saldo -= valor
+                mostrarValorSacado = 'Saque no valor de R${} realizado com sucesso :)'
+                mostrarValorSacado = mostrarValorSacado.format(valor)
+                print(mostrarValorSacado)
+                mostrarSaldo = 'Seu saldo atual é {}'
+                mostrarSaldo = mostrarSaldo.format(saldo)
+                print(mostrarSaldo)
+                break
+
     while loopPrincipal:
         main()
         opcaoMain = input('Digite uma opção: ')
         if opcaoMain == '1':
             mostrarDados()
-        #---------------------------
         elif opcaoMain =='2':
-            if saldo <= 0: # --> passou
+            if saldo <= 0.0: # --> passou
                 print('\nVocê não tem dinheiro suficiente para apostar, deposite uma quantia em R$.')
                 mostrarSaldo = 'Seu saldo atual é de R${}\n'
                 mostrarSaldo = mostrarSaldo.format(saldo)
                 print(mostrarSaldo)
             else:
                 aposta()
-        #--------------------------
         elif opcaoMain == '3':
             depositar()
+        elif opcaoMain == '4':
+            sacar()
         elif opcaoMain == 'S' or opcaoMain == 's':
             print('Volte sempre :)\nSaindo...')
             loopPrincipal = False
+        else:
+            print('Digite um valor válido!')
 
 except:
-    print('Algo deu errado :(')
+    print("Algo deu errado :(")
